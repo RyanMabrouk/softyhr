7033f6b7e9a99982d9d204a8919c97058aed3cb0
Rayen Mabrouk <78177964+RyanMabrouk@users.noreply.github.com>
2023-12-20 11:10:20 +0100

    Revert "Dev"

--- src/api/getValidSubdomain.ts
+++
@@ -1,7 +0,0 @@
-export const getValidSubdomain = (host?: string | null) => {
-  if (!host && typeof window !== "undefined") {
-    host = window.location.host;
-  }
-  const candidate = host?.split(".")[0];
-  return candidate && !candidate.includes("localhost") ? candidate : null;
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import getCurrentorg from "@/api/getCurrentOrg";
 import Image from "next/image";
-import { redirect } from "next/navigation";
 
-export default async function page() {
-  const current_org = await getCurrentorg();
-  if(current_org) redirect('/home');
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-color1-400 p-24 text-color2-100">
-     "landing page"
+      SoftyHr
     </main>
   );
 }
