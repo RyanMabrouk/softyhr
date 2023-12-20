d9d6020b6f0e15916de5c6e219037999c7a2cc64
ihebsebai4444 <ihesebai4444@gmail.com>
2023-12-20 10:09:46 +0100

    feature/sud domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
 
-export default function Home() {
+import Image from "next/image";
+import { headers } from "next/headers";
+import getCurrentorg from "@/api/getCurrentOrg";
+
+export default async function Home() {
+  const current_org = await getCurrentorg();
+  console.log(current_org);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      SoftyHr
+     {current_org?.name}
     </main>
   );
 }
