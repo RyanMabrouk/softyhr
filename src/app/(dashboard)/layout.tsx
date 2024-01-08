aca58c1c1069efbe7503368aa091d4959be658f5
iheb-sebai <ihebsebai4444@gmail.com>
2024-01-08 01:47:04 +0100

    fix: education deleting problem

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import Nav from "../_Layout/_Nav/Nav";
+import Nav from "../_layout/_Nav/Nav";
 import { redirect } from "next/navigation";
 import getSession from "@/actions/getSession";
+import Initialize from "@/provider/Initilize";
 async function Layout({ children }: { children: React.ReactNode }) {
   const session = await getSession();
   if (!session) redirect("/login");
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex h-full w-full min-h-screen flex-col">
+      <Initialize>
       <Nav />
       <main className="h-full min-h-screen w-full ">{children}</main>
+      </Initialize>
     </div>
   );
 }
--- src/app/_ui/Loader/Loader.tsx
+++ src/app/_ui/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import "./Loader.css"
 
 function Loader() {
   return (
-    <div className='w-full h-full flex items justify-center'  >
+    <div className='w-full h-full flex items justify-center m-auto'  >
     <span className='loader'></span>
     </div>
   )
