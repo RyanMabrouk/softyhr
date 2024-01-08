3dbf2eda1ed668b9d53d299c16639b9b9b76bb96
Rayen Mabrouk <mabroukr911@gmail.com>
2024-01-08 12:03:46 +0100

    .

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Nav from "../_Layout/_Nav/Nav";
 import { redirect } from "next/navigation";
 import getSession from "@/api/getSession";
 import Initialize from "@/provider/Initilize";
+import Nav from "../_layout/_Nav/Nav";
 async function Layout({ children }: { children: React.ReactNode }) {
   const session = await getSession();
   if (!session) redirect("/login");
   return (
-    <div className="flex h-full w-full min-h-screen flex-col">
+    <div className="flex h-full min-h-screen w-full flex-col">
       <Initialize>
         <Nav />
         <main className="h-full min-h-screen w-full ">{children}</main>
