fe7a64b420d9eab9e64a5da564651879a0a73f36
Rayen Mabrouk <mabroukr911@gmail.com>
2024-01-09 21:58:13 +0100

    config changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
   logging: {
     fetches: {
       fullUrl: true,
