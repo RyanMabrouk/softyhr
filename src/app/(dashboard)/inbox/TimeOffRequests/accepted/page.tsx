673f2329f97009dc85a5ebe06ccf65d7b2cba752
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-06 13:35:51 +0100

    .

--- src/app/(dashboard)/inbox/TimeOffRequests/accepted/page.tsx
+++ src/app/(dashboard)/inbox/TimeOffRequests/accepted/page.tsx
@@ -6,9 +6,12 @@ import Loader from "@/app/_ui/Loader/Loader";
 import { Player } from "@lottiefiles/react-lottie-player";
 import useAcceptedDeniedLeaves from "@/hooks/TimeOff/useAcceptedDeniedLeaves";
 import { TbPlayerTrackNext } from "react-icons/tb";
+import { useQueryClient } from "@tanstack/react-query";
+import { database_leave_request_status_type } from "@/types/database.tables.types";
+import getAcceptedDeniedLeavs from "@/api/TimeOff/getAcceptedDeniedLeavs";
 export default function Page() {
   const [page, setPage] = React.useState(1);
-  console.log("🚀 ~ Page ~ page:", page);
+  const queryclient = useQueryClient();
   const {
     accepted_denied_leaves: {
       data: accepted_denied_leaves,
@@ -17,6 +20,15 @@ export default function Page() {
   } = useAcceptedDeniedLeaves({
     page: page,
   });
+  if (accepted_denied_leaves?.length === 8 && page) {
+    const status: database_leave_request_status_type = "approved";
+    const status2: database_leave_request_status_type = "rejected";
+    queryclient.prefetchQuery({
+      queryKey: ["leave_requests", status, status2, page + 1],
+      queryFn: () =>
+        getAcceptedDeniedLeavs({ status, status2, page: page + 1 }),
+    });
+  }
   const { data: formatted_leavs, isPending: isPending2 } = useFormattedLeaves({
     leave_requests: accepted_denied_leaves,
   });
@@ -49,19 +61,22 @@ export default function Page() {
         </div>
       )}
       <div className="flex w-full flex-row items-center justify-center">
-        <div className="mt-4 flex w-60 flex-row items-center gap-6">
+        <div className="mt-4 flex w-60 flex-row items-center gap-7">
           <button
             disabled={page === 1}
             onClick={() => setPage((old) => old - 1)}
-            className={`flex cursor-pointer self-start flex-row items-center gap-0.5 text-color5-500 hover:underline disabled:hidden`}
+            className={`flex cursor-pointer flex-row items-center gap-0.5 self-start justify-self-start text-color5-500 hover:underline disabled:hidden`}
           >
             <TbPlayerTrackNext className="-mb-1 rotate-180 text-[0.75rem]" />
             <span>Prev</span>
           </button>
-          <div className="flex flex-row items-center gap-1.5 text-gray-21 self-center">
+          <div
+            className={`flex w-fit flex-row items-center justify-items-center gap-1.5 self-center justify-self-center text-gray-21 ${page === 1 ? "ml-auto" : ""}`}
+          >
             <button
               disabled={page === 1}
-              className="cursor-pointer rounded-sm border border-fabric-700 px-2 py-0.5 text-sm transition-all ease-linear hover:border-white hover:bg-fabric-700 hover:text-white disabled:hidden"
+              onClick={() => setPage((old) => old - 1)}
+              className=" cursor-pointer rounded-sm border border-fabric-700 px-2 py-0.5 text-sm transition-all ease-linear hover:border-white hover:bg-fabric-700 hover:text-white disabled:hidden"
             >
               {page - 1}
             </button>
@@ -70,7 +85,8 @@ export default function Page() {
             </button>
             <button
               disabled={formatted_leavs && formatted_leavs.length < 8}
-              className="cursor-pointer rounded-sm border border-fabric-700 px-2 py-0.5 text-sm transition-all ease-linear hover:border-white hover:bg-fabric-700 hover:text-white disabled:hidden"
+              className=" cursor-pointer rounded-sm border border-fabric-700 px-2 py-0.5 text-sm transition-all ease-linear hover:border-white hover:bg-fabric-700 hover:text-white disabled:hidden"
+              onClick={() => setPage((old) => old + 1)}
             >
               {page + 1}
             </button>
@@ -78,7 +94,7 @@ export default function Page() {
           <button
             disabled={formatted_leavs && formatted_leavs.length < 8}
             onClick={() => setPage((old) => old + 1)}
-            className={`flex cursor-pointer flex-row items-center gap-0.5 text-color5-500 hover:underline disabled:hidden`}
+            className={`flex cursor-pointer flex-row items-center gap-0.5 self-end justify-self-end text-color5-500 hover:underline disabled:hidden`}
           >
             <span>Next</span>
             <TbPlayerTrackNext className="-mb-1 text-[0.75rem]" />
