65d757af6fac86e834054d3db1285bc8be782689
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-06 13:35:51 +0100

    .

--- src/api/TimeOff/getAcceptedDeniedLeavs.tsx
+++ src/api/TimeOff/getAcceptedDeniedLeavs.tsx
@@ -17,14 +17,15 @@ export default async function getAcceptedDeniedLeavs({
     data: { session },
   } = await supabase.auth.getSession();
   const org_name = session?.user.user_metadata.org_name;
+  const start = page === 1 ? 0 : (page - 1) * 8 + page;
+  console.log("🚀 ~ start:", start);
+  const end = page === 1 ? page * 8 : page * 8 + (page + 1);
+  console.log("🚀 ~ end:", end);
   const { data, error } = await supabase
     .from("leave_requests")
     .select("*")
     .filter("status", "in", `("${status}","${status2}")`)
     .match({ org_name: org_name })
-    .range(
-      page === 1 ? 0 : (page - 1) * 8 + 1,
-      page === 1 ? page * 8 : page * 8 + 1,
-    );
+    .range(start, end);
   return { data: data, error: error };
 }
