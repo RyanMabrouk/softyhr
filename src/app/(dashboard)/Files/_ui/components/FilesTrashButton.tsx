7d8fe6bb3b9013713d302796d96326a6c1c59899
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-13 21:13:31 +0100

    Refactor FilesTrashButton component

--- src/app/(dashboard)/Files/_ui/components/FilesTrashButton.tsx
+++ src/app/(dashboard)/Files/_ui/components/FilesTrashButton.tsx
@@ -1,32 +1,20 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-
 import { FaTrash } from "react-icons/fa";
 import { useQueryClient } from "@tanstack/react-query";
-import useData from "@/hooks/useData";
 import RoleGuard from "@/app/_ui/RoleGuard";
-
 export default function FilesTrashButton({ check }: any) {
-  const {
-    user_profile: { data: cur_user, isPending: isPending_user },
-  } = useData();
-
-  const role = cur_user?.role;
-
   const Router = useRouter();
   const queryClient = useQueryClient();
   const [isDisabled, setIsDisabled] = useState(true);
-
   function handleMouseEnter() {
     const data: any = queryClient.getQueryData(["fileIds"]);
     setIsDisabled(data === undefined || data?.length === 0);
   }
-
   function handleClick() {
     if (!isDisabled) Router.replace("Files?popup=DELETE_FILES");
   }
-
   return (
     <RoleGuard permissions={["delete:files"]}>
       <button
