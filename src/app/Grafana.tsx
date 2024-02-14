37b86a3930c8537f7249cc5350b1e3a5d777d012
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-14 15:20:03 +0100

    Fix pagination component and remove unused code

--- src/app/Grafana.tsx
+++ src/app/Grafana.tsx
@@ -19,8 +19,6 @@ function Grafana() {
       });
     }
   }, []);
-
-  // ...
   return null;
 }
 export default Grafana;
---
+++ src/components/ui/Pagination.tsx
@@ -0,0 +1,66 @@
+import { PAGE_SIZE } from "@/constants/filesConstants";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+
+function Pagination({
+  count,
+  page,
+  handlePage,
+}: {
+  count: number;
+  page: number;
+  handlePage: (page: number) => void;
+}) {
+  const currentPage = page;
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1;
+
+    handlePage(next);
+  }
+
+  function prevPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1;
+
+    handlePage(prev);
+  }
+
+  if (pageCount <= 1) return null;
+
+  return (
+    <div className="absolute bottom-3 mt-8 flex w-full items-center justify-between">
+      <p className=" flex flex-row gap-1 text-gray-25">
+        Showing
+        <span className=" font-semibold text-color-primary-8">
+          {(currentPage - 1) * PAGE_SIZE + 1}
+        </span>
+        to
+        <span className=" font-semibold text-color-primary-8">
+          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
+        </span>
+        of <span className=" font-semibold text-color-primary-8">{count}</span>{" "}
+        results
+      </p>
+
+      <div className="flex gap-2">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className={`flex items-center disabled:text-gray-25 disabled:border-gray-25 border border-transparent justify-center gap-2 rounded bg-fabric-700 px-3 py-1 text-white transition-all ease-linear disabled:bg-white ${currentPage === 1 ? "cursor-not-allowed" : "hover:bg-fabric-800"}`}
+        >
+          <HiChevronLeft />
+        </button>
+
+        <button
+          onClick={nextPage}
+          disabled={currentPage === pageCount}
+          className={`flex items-center disabled:text-gray-25 disabled:border-gray-25 border border-transparent justify-center gap-2 rounded bg-fabric-700 px-3 py-1 text-white transition-all ease-linear disabled:bg-white ${currentPage === pageCount ? "cursor-not-allowed" : "hover:bg-fabric-800"}`}
+        >
+          <HiChevronRight />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Pagination;
