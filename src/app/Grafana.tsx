751740e0b5d5300a623239adaea886d8c87347d2
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-15 09:45:26 +0100

    Update package.json and Grafana.tsx

--- src/app/Grafana.tsx
+++ src/app/Grafana.tsx
@@ -4,20 +4,18 @@ import { getWebInstrumentations, initializeFaro } from "@grafana/faro-web-sdk";
 import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 function Grafana() {
   useEffect(() => {
-    if (!(window as any).faro) {
-      initializeFaro({
-        url: "https://faro-collector-prod-eu-west-2.grafana.net/collect/9d533f339dc67a61608a70148d225f63",
-        app: {
-          name: "softyhr",
-          version: "1.0.0",
-          environment: "production",
-        },
-        instrumentations: [
-          ...getWebInstrumentations(),
-          new TracingInstrumentation(),
-        ],
-      });
-    }
+    initializeFaro({
+      url: "https://faro-collector-prod-eu-west-2.grafana.net/collect/9d533f339dc67a61608a70148d225f63",
+      app: {
+        name: "softyhr",
+        version: "1.0.0",
+        environment: "production",
+      },
+      instrumentations: [
+        ...getWebInstrumentations(),
+        new TracingInstrumentation(),
+      ],
+    });
   }, []);
   return null;
 }
