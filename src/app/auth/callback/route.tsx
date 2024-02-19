e7c84935176ab61efa8cad41f4107408e9e54e37
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-19 14:46:17 +0100

    Update dependencies and refactor API routes

--- src/app/auth/callback/route.tsx
+++ src/app/auth/callback/route.tsx
@@ -12,34 +12,27 @@ export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   try {
-    if (code) {
-      const supabase = createRouteHandlerClient({ cookies });
-      await supabase.auth.exchangeCodeForSession(code);
-      //get user company name
-      const session = await getSession();
-      const { data: profiles, error } = await getData("profiles");
-      const company = profiles?.filter(
-        (profile: database_profile_type) =>
-          profile.user_id === session?.user.id,
-      )?.[0]?.org_name;
-      if (error) throw new Error(error.message);
-      //sign out from the landing page url
-      const { error: signout_error } = await supabase.auth.signOut();
-      if (signout_error) throw new Error(signout_error.message);
-      if (!company) throw new Error("company is not defined");
-      // URL to redirect to after sign in process completes
-      //logger.info("intercepted in caalback api route_exit");
-      const redirectUrl = `${requestUrl.protocol}//${company}.${requestUrl.host}/home`;
-      console.log("🚀 ~ GET ~ redirectUrl:", redirectUrl);
-      return NextResponse.redirect(redirectUrl);
-    } else {
+    if (!code) {
       const error = requestUrl.searchParams.get("error");
       const error_description =
         requestUrl.searchParams.get("error_description");
-      // logger.error(error + " discription :" + error_description);
       console.error(error + " discription :" + error_description);
       throw new Error("code is not defined");
     }
+    const supabase = createRouteHandlerClient({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
+    const session = await getSession();
+    const { data: profiles, error } = await getData("profiles");
+    const company = profiles?.filter(
+      (profile: database_profile_type) => profile.user_id === session?.user.id,
+    )?.[0]?.org_name;
+    if (error) throw new Error(error.message);
+    const { error: signout_error } = await supabase.auth.signOut();
+    if (signout_error) throw new Error(signout_error.message);
+    if (!company) throw new Error("company is not defined");
+    const redirectUrl = `${requestUrl.protocol}//${company}.${requestUrl.host}/home`;
+    console.log("🚀 ~ GET ~ redirectUrl:", redirectUrl);
+    return NextResponse.redirect(redirectUrl);
   } catch (error) {
     //logger.error(error);
     // URL to redirect if sign in process failes
