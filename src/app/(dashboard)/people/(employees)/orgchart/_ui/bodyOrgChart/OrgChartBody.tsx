a80ee63ed0dc437543b90179bc391780473e6426
Rayen Mabrouk <mabroukr911@gmail.com>
2024-02-21 17:56:55 +0100

    Add dependencies and update OrgChartBody component

--- src/app/(dashboard)/people/(employees)/orgchart/_ui/bodyOrgChart/OrgChartBody.tsx
+++ src/app/(dashboard)/people/(employees)/orgchart/_ui/bodyOrgChart/OrgChartBody.tsx
@@ -7,37 +7,33 @@ import OrgChartComponent from "../components/OrgChart/OrgChart";
 const OrgChartBody = () => {
   const { profiles } = useProfilesOrgChart();
   const isPending: any = profiles.isPending;
-  const wantedData: any = [];
-
   const allProfiles = !isPending ? profiles?.data : null;
-  !isPending &&
-    allProfiles?.forEach((prof: any) => {
-      wantedData.push({
-        name:
-          prof?.["Basic Information"]?.["First name"] +
-          prof?.["Basic Information"]?.["Last name"],
-        imageUrl: `${
-          prof?.picture
-            ? prof?.picture
-            : "https://raw.githubusercontent.com/bumbeishvili/Assets/master/Projects/D3/Organization%20Chart/general.jpg"
-        }`,
-        profileUrl: "http://facebook.com",
-        office: prof?.["Job Information"]?.[0]?.["Department"],
-        tags: "CEO",
-        isLoggedUser: false,
-        positionName: prof?.["Job Information"]?.[0]?.["Job Title"],
-        id: prof?.user_id ? prof?.user_id : "",
-        parentId: prof?.supervisor_id,
-        size: "",
-      });
-    });
+  const data =
+    allProfiles?.map((prof: any) => ({
+      name:
+        prof?.["Basic Information"]?.["First name"] +
+        prof?.["Basic Information"]?.["Last name"],
+      imageUrl: `${
+        prof?.picture
+          ? prof?.picture
+          : "https://raw.githubusercontent.com/bumbeishvili/Assets/master/Projects/D3/Organization%20Chart/general.jpg"
+      }`,
+      profileUrl: "http://facebook.com",
+      office: prof?.["Job Information"]?.[0]?.["Department"],
+      tags: "CEO",
+      isLoggedUser: false,
+      positionName: prof?.["Job Information"]?.[0]?.["Job Title"],
+      id: prof?.user_id ? prof?.user_id : "",
+      parentId: prof?.supervisor_id,
+      size: "",
+    })) ?? [];
   return (
     <>
       {isPending ? (
         <LoaderFiles />
       ) : (
         <div className="mt-10 ">
-          <OrgChartComponent data={wantedData} />
+          <OrgChartComponent data={data} />
         </div>
       )}
     </>
