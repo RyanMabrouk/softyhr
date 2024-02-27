c0f55df11d7755653575806e4348a20358aaa745
iheb4444 <ihebsebai4444@gmail.com>
2024-02-27 17:00:44 +0100

    fix build error

--- src/app/(dashboard)/Hiring/jobs/[Job_id]/components/CandidatesTable/components/Settings/Settings.tsx
+++ src/app/(dashboard)/Hiring/jobs/[Job_id]/components/CandidatesTable/components/Settings/Settings.tsx
@@ -41,7 +41,7 @@ function Settings() {
     >
       <div
         id="settings"
-        className="flex cursor-pointer flex-row items-center justify-center gap-1 border border-gray-25 px-2 py-1.5 shadow-sm transition-all ease-linear hover:shadow-md"
+        className="flex h-[2.2rem] w-[3rem] cursor-pointer flex-row items-center justify-center gap-1 border border-gray-25 px-2 py-1.5 shadow-sm transition-all ease-linear hover:shadow-md"
       >
         <IoMdSettings className="h-6 w-6 text-gray-25" />
         <VscTriangleDown className="h-3 w-3 text-gray-25" />
